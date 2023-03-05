a0ff48e3bcfca284794664aca398aa5b6db89b42
Petr Kracík <petrkr@petrkr.net>
2023-03-05 01:17:49 +0100

    Update test to new interface

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
 from serial import Serial
 import ds2480
 
+def print_bytearray(data):
+    for b in data:
+        print("0x{:02X}".format(b))
+
+
+def read_rom(ds):
+    print("Read ROM")
+    print(ds.write(0x33))
+
+    while True:
+        response = ds.read()
+        if response == 0xFF:
+            break
+
+        print("0x{:02X}".format(response))
+
+
 s = Serial("/dev/ttyACM0", 9600, timeout=1)
 ds = ds2480.DS2480(s)
 
 print(ds.reset())
 
-print("Write")
-print(ds.write(0x33))
+read_rom(ds)
 
-while True:
-    response = ds.read()
-    if response == 0xFF:
-        break
+print("Search ROMs")
+devs = ds.search()
 
-    print("0x{:02X}".format(response))
-
-print(ds.reset())
+for dev in devs:
+    print_bytearray(dev)
+    print()
